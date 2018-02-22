5a229e10d63d0cc1e2ea7ac2e4337f2d58549f27
Esther Weon <esther.s.weon@gmail.com>
2018-02-22 11:05:53 -0800

    Add comments

--- src/App.js
+++ src/App.js
@@ -11,11 +11,17 @@ class App extends Component {
           <h1 className="App-title">I am an app</h1>
         </header>
         <Accordion>
+          {/* Everything between opening / closing tag of Accordion is considered its children */}
+          {/* You can access this using `this.props.children` in the Accordion component */ }
+          
+          {/* This div is the first child of this Accordion */}
           <div>
             <h1>SECTION 1</h1>
             <p>This is a section of the accordion.</p>
             <p>It has multiple tags inside this one section.</p>
           </div>
+
+          {/* This div is the second child of this Accordion */}
           <div>
             <h1>SECTION 2</h1>
             <p>Each section can have whatever it wants – does not have to follow same format.</p>
@@ -23,12 +29,17 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
           </div>
         </Accordion>
+
+        {/* This is another accordion */}
         <Accordion>
+          {/* This div is the first child of this Accordion */}
           <div>
             <h1>ANOTHER SECTION 1</h1>
             <p>This is a section of another accordion.</p>
             <img src={logo} className="App-logo" alt="logo" />
           </div>
+
+          {/* This div is the first child of this Accordion */}
           <div>
             <h1>ANOTHER SECTION 2</h1>
             <p>Still customizable!</p>
